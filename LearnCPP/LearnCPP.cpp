582d031f8e4d11b04b8b2d4ae4b4b50558e60fb2
Marcel Triebe <M4rcel2012@gmail.com>
2026-04-10 15:45:52 +0200

    Object Relationship

--- LearnCPP/LearnCPP.cpp
+++ LearnCPP/LearnCPP.cpp
@@ -7,6 +7,7 @@
 #include "8xe.h"
 #include "Creature.h"
 #include "Point2D.h"
+#include "IntArray.h"
 
 #include <iostream>
 #include <iomanip>
@@ -2143,6 +2144,283 @@ namespace MoveConstructor
 	//!!!The implicit move constructor and move assignment will copy pointers, not move them. If you want to move a pointer member, you will need to define the move constructor and move assignment yourself.!!
 }
 
+namespace ObjectCompositions
+{
+	//_________________Aggregation_______________________________________
+	class Teacher
+	{
+	private:
+		std::string m_name{};
+
+	public:
+		Teacher(std::string_view name)
+			: m_name{ name }
+		{
+		}
+
+		const std::string& getName() const { return m_name; }
+	};
+
+	class Department
+	{
+	private:
+		const Teacher& m_teacher; // This dept holds only one teacher for simplicity, but it could hold many teachers
+
+	public:
+		Department(const Teacher& teacher)
+			: m_teacher{ teacher }
+		{
+		}
+	};
+
+	//int main()
+	//{
+	//	// Create a teacher outside the scope of the Department
+	//	Teacher bob{ "Bob" }; // create a teacher
+
+	//	{
+	//		// Create a department and use the constructor parameter to pass
+	//		// the teacher to it.
+	//		Department department{ bob };
+
+	//	} // department goes out of scope here and is destroyed
+
+	//	// bob still exists here, but the department doesn't
+
+	//	std::cout << bob.getName() << " still exists!\n";
+
+	//	return 0;
+	//}
+
+	//___________________________________Association_____________________________________________
+	// Since Doctor and Patient have a circular dependency, we're going to forward declare Patient
+	class Patient;
+
+	class Doctor
+	{
+	private:
+		std::string m_name{};
+		std::vector<std::reference_wrapper<const Patient>> m_patient{};
+
+	public:
+		Doctor(std::string_view name) :
+			m_name{ name }
+		{
+		}
+
+		void addPatient(Patient& patient);
+
+		// We'll implement this function below Patient since we need Patient to be defined at that point
+		friend std::ostream& operator<<(std::ostream& out, const Doctor& doctor);
+
+		const std::string& getName() const { return m_name; }
+	};
+
+	class Patient
+	{
+	private:
+		std::string m_name{};
+		std::vector<std::reference_wrapper<const Doctor>> m_doctor{}; // so that we can use it here
+
+		// We're going to make addDoctor private because we don't want the public to use it.
+		// They should use Doctor::addPatient() instead, which is publicly exposed
+		void addDoctor(const Doctor& doctor)
+		{
+			m_doctor.push_back(doctor);
+		}
+
+	public:
+		Patient(std::string_view name)
+			: m_name{ name }
+		{
+		}
+
+		// We'll implement this function below to parallel operator<<(std::ostream&, const Doctor&)
+		friend std::ostream& operator<<(std::ostream& out, const Patient& patient);
+
+		const std::string& getName() const { return m_name; }
+
+		// We'll friend Doctor::addPatient() so it can access the private function Patient::addDoctor()
+		friend void Doctor::addPatient(Patient& patient);
+	};
+
+	void Doctor::addPatient(Patient& patient)
+	{
+		// Our doctor will add this patient
+		m_patient.push_back(patient);
+
+		// and the patient will also add this doctor
+		patient.addDoctor(*this);
+	}
+
+	std::ostream& operator<<(std::ostream& out, const Doctor& doctor)
+	{
+		if (doctor.m_patient.empty())
+		{
+			out << doctor.m_name << " has no patients right now";
+			return out;
+		}
+
+		out << doctor.m_name << " is seeing patients: ";
+		for (const auto& patient : doctor.m_patient)
+			out << patient.get().getName() << ' ';
+
+		return out;
+	}
+
+	std::ostream& operator<<(std::ostream& out, const Patient& patient)
+	{
+		if (patient.m_doctor.empty())
+		{
+			out << patient.getName() << " has no doctors right now";
+			return out;
+		}
+
+		out << patient.m_name << " is seeing doctors: ";
+		for (const auto& doctor : patient.m_doctor)
+			out << doctor.get().getName() << ' ';
+
+		return out;
+	}
+
+	//int main()
+	//{
+	//	// Create a Patient outside the scope of the Doctor
+	//	Patient dave{ "Dave" };
+	//	Patient frank{ "Frank" };
+	//	Patient betsy{ "Betsy" };
+
+	//	Doctor james{ "James" };
+	//	Doctor scott{ "Scott" };
+
+	//	james.addPatient(dave);
+
+	//	scott.addPatient(dave);
+	//	scott.addPatient(betsy);
+
+	//	std::cout << james << '\n';
+	//	std::cout << scott << '\n';
+	//	std::cout << dave << '\n';
+	//	std::cout << frank << '\n';
+	//	std::cout << betsy << '\n';
+
+	//	return 0;
+	//}
+
+	//Use only if needed (space or speed)
+	class Car
+	{
+	private:
+		std::string m_name{};
+		int m_id{};
+
+	public:
+		Car(std::string_view name, int id)
+			: m_name{ name }, m_id{ id }
+		{
+		}
+
+		const std::string& getName() const { return m_name; }
+		int getId() const { return m_id; }
+	};
+
+	// Our CarLot is essentially just a static array of Cars and a lookup function to retrieve them.
+	// Because it's static, we don't need to allocate an object of type CarLot to use it
+	namespace CarLot
+	{
+		Car carLot[4]{ { "Prius", 4 }, { "Corolla", 17 }, { "Accord", 84 }, { "Matrix", 62 } };
+
+		Car* getCar(int id)
+		{
+			for (auto& car : carLot)
+			{
+				if (car.getId() == id)
+				{
+					return &car;
+				}
+			}
+
+			return nullptr;
+		}
+	};
+
+	class Driver
+	{
+	private:
+		std::string m_name{};
+		int m_carId{}; // we're associated with the Car by ID rather than pointer
+
+	public:
+		Driver(std::string_view name, int carId)
+			: m_name{ name }, m_carId{ carId }
+		{
+		}
+
+		const std::string& getName() const { return m_name; }
+		int getCarId() const { return m_carId; }
+	};
+
+	//int main()
+	//{
+	//	Driver d{ "Franz", 17 }; // Franz is driving the car with ID 17
+
+	//	Car* car{ CarLot::getCar(d.getCarId()) }; // Get that car from the car lot
+
+	//	if (car)
+	//		std::cout << d.getName() << " is driving a " << car->getName() << '\n';
+	//	else
+	//		std::cout << d.getName() << " couldn't find his car\n";
+
+	//	return 0;
+	//}
+}
+
+namespace ContainerClass
+{
+//#include "IntArray.h"
+
+	//int main()
+	//{
+	//	// Declare an array with 10 elements
+	//	IntArray array(10);
+
+	//	// Fill the array with numbers 1 through 10
+	//	for (int i{ 0 }; i < 10; ++i)
+	//		array[i] = i + 1;
+
+	//	// Resize the array to 8 elements
+	//	array.resize(8);
+
+	//	// Insert the number 20 before element with index 5
+	//	array.insertBefore(20, 5);
+
+	//	// Remove the element with index 3
+	//	array.remove(3);
+
+	//	// Add 30 and 40 to the end and beginning
+	//	array.insertAtEnd(30);
+	//	array.insertAtBeginning(40);
+
+	//	// A few more tests to ensure copy constructing / assigning arrays
+	//	// doesn't break things
+	//	{
+	//		IntArray b{ array };
+	//		b = array;
+	//		b = b;
+	//		array = array;
+	//	}
+
+	//	// Print out all the numbers
+	//	for (int i{ 0 }; i < array.getLength(); ++i)
+	//		std::cout << array[i] << ' ';
+
+	//	std::cout << '\n';
+
+	//	return 0;
+	//}
+}
+
+
 
 
 
@@ -2152,7 +2430,7 @@ int main()
 {
 	namespace CTL = CppLearningTest;
 	
-
+ 
 
 	return 0;
 
