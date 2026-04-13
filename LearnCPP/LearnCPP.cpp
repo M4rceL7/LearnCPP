b1799106b2fe845520f7584b2074233c61b14778
Marcel Triebe <M4rcel2012@gmail.com>
2026-04-13 16:25:07 +0200

    start to inheritance

--- LearnCPP/LearnCPP.cpp
+++ LearnCPP/LearnCPP.cpp
@@ -2421,6 +2421,63 @@ namespace ContainerClass
 }
 
 
+namespace Inheritance
+{
+	class Person
+	{
+		// In this example, we're making our members public for simplicity
+	public:
+		std::string m_name{};
+		int m_age{};
+
+		Person(std::string_view name = "", int age = 0)
+			: m_name{ name }, m_age{ age }
+		{
+		}
+
+		const std::string& getName() const { return m_name; }
+		int getAge() const { return m_age; }
+
+	};
+
+	// BaseballPlayer publicly inheriting Person
+	class BaseballPlayer : public Person
+	{
+	public:
+		double m_battingAverage{};
+		int m_homeRuns{};
+
+		BaseballPlayer(double battingAverage = 0.0, int homeRuns = 0)
+			: m_battingAverage{ battingAverage }, m_homeRuns{ homeRuns }
+		{
+		}
+	};
+
+	// Employee publicly inherits from Person
+	class Employee : public Person
+	{
+	public:
+		double m_hourlySalary{};
+		long m_employeeID{};
+
+		Employee(double hourlySalary = 0.0, long employeeID = 0)
+			: m_hourlySalary{ hourlySalary }, m_employeeID{ employeeID }
+		{
+		}
+
+		void printNameAndSalary() const
+		{
+			std::cout << m_name << ": " << m_hourlySalary << '\n';
+		}
+	};
+
+	class Supervisor : public Employee
+	{
+	public:
+		// This Supervisor can oversee a max of 5 employees
+		long m_overseesIDs[5]{};
+	};
+}
 
 
 
