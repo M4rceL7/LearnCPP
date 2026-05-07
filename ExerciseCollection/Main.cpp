33764889563a00d6868268f5729a391dd65684c6
Marcel Triebe <M4rcel2012@gmail.com>
2026-05-07 14:19:54 +0200

    Added Chapter 1.x

--- ExerciseCollection/Main.cpp
+++ ExerciseCollection/Main.cpp
@@ -1,9 +1,33 @@
+#include <iostream>
+
 //Here will be every exercise from every Chapter Summary with a (Quiz) exercise.
 //Mainly done without copying the solution.
+
+namespace _1x{
+
+	/*int main()
+	{
+
+		std::cout << "Enter an integer: ";
+		int number1{};
+		std::cin >> number1;
+
+		std::cout << "Enter another integer: ";
+		int number2{};
+		std::cin >> number2;
+
+		std::cout << number1 << " + " << number2 << " is " << number1 + number2 << ".\n";
+		std::cout << number1 << " - " << number2 << " is " << number1 - number2 << ".\n";
+
+		return 0;
+	}*/
+}
+
 
 int main()
 {
 
+	
 
 	return 0;
 }
